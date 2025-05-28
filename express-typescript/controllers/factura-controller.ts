826c9d0bd72ef7791422aa6c287d88607d8785ad
Sena <dimatesebas73@gmail.com>
2025-05-28 16:48:23 -0500

    Logica de la pasarela de pagos

--- express-typescript/controllers/factura-controller.ts
+++ express-typescript/controllers/factura-controller.ts
@@ -10,6 +10,7 @@ export const guardarFactura = async (req: Request) => {
     x_currency_code,
     x_franchise,
     x_xextra1,
+    x_xextra2,
   } = req.body;
 
   if (!x_xextra1 || isNaN(parseInt(x_xextra1))) {
@@ -26,8 +27,9 @@ export const guardarFactura = async (req: Request) => {
       valor,
       moneda,
       metodo_pago,
-      id_usuario
-    ) VALUES (?, ?, ?, ?, ?, ?, ?)
+      id_usuario,
+      contenido_factura
+    ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
   `;
 
   await db.query(sql, [
@@ -38,13 +40,29 @@ export const guardarFactura = async (req: Request) => {
     x_currency_code,
     x_franchise,
     id_usuario,
+    x_xextra2
   ]);
 };
 
 
 
 export const obtenerFacturas = async (req: any, res: any) => {
-    
-    const result = await db.query('SELECT * FROM facturas WHERE id_usuario = ?');
-    res.json(result);
+  try {
+    const [rows]: any = await db.query(
+      `SELECT * FROM facturas ORDER BY id DESC`
+    );
+
+    const facturas = rows.map((factura: any) => ({
+      ...factura,
+      contenido_factura: typeof factura.contenido_factura === 'string'
+        ? JSON.parse(factura.contenido_factura)
+        : factura.contenido_factura,
+    }));
+
+    res.json(facturas);
+  } catch (error) {
+    console.error("Error al obtener todas las facturas:", error);
+    res.status(500).json({ message: 'Error al obtener las facturas' });
   }
+}
+
